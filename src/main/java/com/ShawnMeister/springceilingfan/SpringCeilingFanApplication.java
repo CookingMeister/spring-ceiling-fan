d1f7321b0d0d28e4f204aa3948904c80ae1c98d0
Shawn Meister <ss.meister@hotmail.com>
2024-05-17 14:59:14 -0400

    add initializr files for project

---
+++ src/main/java/com/ShawnMeister/springceilingfan/SpringCeilingFanApplication.java
@@ -0,0 +1,14 @@
+package com.ShawnMeister.springceilingfan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCeilingFanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCeilingFanApplication.class, args);
+		System.out.println("Hello, I am a big fan");
+	}
+
+}
---
+++ src/test/java/com/ShawnMeister/springceilingfan/SpringCeilingFanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ShawnMeister.springceilingfan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCeilingFanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
