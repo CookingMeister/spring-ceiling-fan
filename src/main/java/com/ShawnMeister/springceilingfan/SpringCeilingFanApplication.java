8f917458d4bafb5fec6b8f1cadebf813dfbdd337
Shawn Meister <ss.meister@hotmail.com>
2024-05-17 17:03:44 -0400

    add KeyEventHandler class, createKeyListener method, not working

--- src/main/java/com/ShawnMeister/springceilingfan/SpringCeilingFanApplication.java
+++ src/main/java/com/ShawnMeister/springceilingfan/SpringCeilingFanApplication.java
@@ -2,7 +2,6 @@ package com.ShawnMeister.springceilingfan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import com.ShawnMeister.springceilingfan.CeilingFan;
 
 @SpringBootApplication
 public class SpringCeilingFanApplication {
@@ -10,8 +9,9 @@ public class SpringCeilingFanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCeilingFanApplication.class, args);
 		System.out.println("Hello, I am a big fan");
-		CeilingFan fan = new CeilingFan();
-		fan.pullSpeedCord();
+		CeilingFan ceilingFan = new CeilingFan();
+		KeyEventHandler keyEventHandler = new KeyEventHandler(ceilingFan);
+		keyEventHandler.createKeyListener();
 	}
 
 }
