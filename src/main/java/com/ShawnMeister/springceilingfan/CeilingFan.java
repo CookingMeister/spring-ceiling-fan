4372aafd80c1dd1280185f33bb3670ec422c8686
Shawn Meister <ss.meister@hotmail.com>
2024-05-17 15:53:51 -0400

    add CeilingFan class, pullSpeedCord, pullDirectionCord, isDecember25 methods

---
+++ src/main/java/com/ShawnMeister/springceilingfan/CeilingFan.java
@@ -0,0 +1,32 @@
+package com.ShawnMeister.springceilingfan;
+
+import java.time.LocalDate;
+
+public class CeilingFan {
+    private int speed = 0;
+    private boolean reversed = false;
+
+    public void pullSpeedCord() {
+        if (!isDecember25th()) {
+            speed = (speed + 1) % 4;
+            System.out.println("Speed: " + speed + ", Direction: " + (reversed ? "Reversed" : "Forward"));
+        } else {
+            System.out.println("Ceiling fan is disabled on December 25th");
+        }
+    }
+
+    public void pullDirectionCord() {
+        if (!isDecember25th()) {
+            reversed = !reversed;
+            System.out.println("Speed: " + speed + ", Direction: " + (reversed ? "Reversed" : "Forward"));
+        } else {
+            System.out.println("Ceiling fan is disabled on December 25th");
+        }
+    }
+
+    private boolean isDecember25th() {
+        LocalDate today = LocalDate.now();
+        System.out.println("Today is: " + today);
+        return today.getMonthValue() == 12 && today.getDayOfMonth() == 25;
+    }
+}
--- src/main/java/com/ShawnMeister/springceilingfan/SpringCeilingFanApplication.java
+++ src/main/java/com/ShawnMeister/springceilingfan/SpringCeilingFanApplication.java
@@ -2,6 +2,7 @@ package com.ShawnMeister.springceilingfan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import com.ShawnMeister.springceilingfan.CeilingFan;
 
 @SpringBootApplication
 public class SpringCeilingFanApplication {
@@ -9,6 +10,8 @@ public class SpringCeilingFanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCeilingFanApplication.class, args);
 		System.out.println("Hello, I am a big fan");
+		CeilingFan fan = new CeilingFan();
+		fan.pullSpeedCord();
 	}
 
 }
